f9933becfa40f409f547d7ea6625cdd3ae189d58
Mayelle Carvalho <carvalhomayelle@gmail.com>
2019-01-03 10:39:52 -0300

    Configurações do django admin super usuario

--- flash/comum/admin.py
+++ flash/comum/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Perfil, Comentario, Post, Categoria, Colecao, Comunidade
+
+admin.site.register(Perfil)
+admin.site.register(Post)
+admin.site.register(Comentario)
+admin.site.register(Categoria)
+admin.site.register(Colecao)
+admin.site.register(Comunidade)
